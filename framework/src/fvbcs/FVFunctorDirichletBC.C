375351d9f6e88f53d42166ab95cee82868a68f28
tanome <mauriciotano@gmail.com>
2024-10-24 16:51:52 -0600

    style changes required by code pre-check and removing wasp Refs #28891

--- framework/src/fvbcs/FVFunctorDirichletBC.C
+++ framework/src/fvbcs/FVFunctorDirichletBC.C
@@ -58,11 +58,13 @@ FVFunctorDirichletBCTempl<is_ad>::boundaryValue(const FaceInfo & fi) const
   if (!_use_other_side)
     return _functor(sfa, determineState());
   else if (fi.elemPtr() == sfa.face_side)
-    return _functor({&fi, Moose::FV::LimiterType::CentralDifference, true, false, fi.neighborPtr(), nullptr},
-                    determineState());
+    return _functor(
+        {&fi, Moose::FV::LimiterType::CentralDifference, true, false, fi.neighborPtr(), nullptr},
+        determineState());
   else
-    return _functor({&fi, Moose::FV::LimiterType::CentralDifference, true, false, fi.elemPtr(), nullptr},
-                    determineState());
+    return _functor(
+        {&fi, Moose::FV::LimiterType::CentralDifference, true, false, fi.elemPtr(), nullptr},
+        determineState());
 }
 
 template class FVFunctorDirichletBCTempl<false>;
