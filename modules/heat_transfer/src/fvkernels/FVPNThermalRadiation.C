076279a9078935d398d1ffb01c8c2cb1024979f4
tanome <mauriciotano@gmail.com>
2025-03-12 17:15:28 -0600

    adding generalized Marshak BC and P1 and P3 examples Refs #0

--- modules/heat_transfer/src/fvkernels/FVPNThermalRadiation.C
+++ modules/heat_transfer/src/fvkernels/FVPNThermalRadiation.C
@@ -7,53 +7,65 @@
 //* Licensed under LGPL 2.1, please see LICENSE for details
 //* https://www.gnu.org/licenses/lgpl-2.1.html
 
-#include "PNThermalRadiation.h"
+#include "FVPNThermalRadiation.h"
 #include "MathUtils.h"
 #include "HeatConductionNames.h"
 
-registerMooseObject("HeatTransferApp", PNThermalRadiation);
+registerMooseObject("HeatTransferApp", FVPNThermalRadiation);
 
 InputParameters
-PNThermalRadiation::validParams()
+FVPNThermalRadiation::validParams()
 {
-  InputParameters params = FVDiffusion::validParams();
+  InputParameters params = FVFluxKernel::validParams();
   params.addClassDescription(
       "Kernel that assembles the equations for PN thermal radiation transport.");
 
   params.addRequiredParam<unsigned int>("n", "N-th Order of the Equation.");
 
-  params.addRequiredParam<MooseFunctorName>(
-      "phi_n_minus_2", "Lagged order of the radiation flux.");
+  params.addParam<MooseFunctorName>(
+      "phi_n_minus_2", 0.0, "Lagged order of the radiation flux.");
   params.addParam<MooseFunctorName>(
       "phi_n_plus_2", 0.0, "Leading order of the radiation flux.");
 
-  params.addRequiredParam<MooseFunctorName>(
-        "sigma_n_minus_1", "Lagged order of the radiation flux.");
+  params.addParam<MooseFunctorName>(
+        "sigma_n_minus_1", 1.0, "Lagged order of the radiation flux.");
   params.addParam<MooseFunctorName>(
         "sigma_n_plus_1", 1.0, "Leading order of the radiation flux.");
+
+  MooseEnum coeff_interp_method("average harmonic", "harmonic");
+  params.addParam<MooseEnum>(
+      "coeff_interp_method",
+      coeff_interp_method,
+      "Switch that can select face interpolation method for diffusion coefficients.");
 
   return params;
 }
 
-PNThermalRadiation::PNThermalRadiation(const InputParameters & params)
-  : FVDiffusion(params),
+FVPNThermalRadiation::FVPNThermalRadiation(const InputParameters & params)
+  : FVFluxKernel(params),
     _n(getParam<unsigned int>("n")),
     _phi_n_minus_2(getFunctor<ADReal>("phi_n_minus_2")),
     _phi_n_plus_2(getFunctor<ADReal>("phi_n_plus_2")),
     _sigma_n_minus_1(getFunctor<ADReal>("sigma_n_minus_1")),
     _sigma_n_plus_1(getFunctor<ADReal>("sigma_n_plus_1"))
 {
+  const auto & interp_method = getParam<MooseEnum>("coeff_interp_method");
+  if (interp_method == "average")
+    _coeff_interp_method = Moose::FV::InterpMethod::Average;
+  else if (interp_method == "harmonic")
+    _coeff_interp_method = Moose::FV::InterpMethod::HarmonicAverage;
 }
 
 ADReal
-PNThermalRadiation::computeQpResidual()
+FVPNThermalRadiation::computeQpResidual()
 {
   using namespace Moose::FV;
   const auto state = determineState();
   const auto elem_arg = elemArg();
+  const auto rn = Real(_n);
 
   // Building the residual for the leading order
-  const auto n_order_plus_2 = (_n+1)*(_n+2)/((2*_n+1)*(2*_n+3));
+  const auto n_order_plus_2 = (rn+1)*(rn+2)/((2*rn+1)*(2*rn+3));
   ADReal sigma_n_plus_1_interpolated;
   interpolate(_coeff_interp_method,
     sigma_n_plus_1_interpolated,
@@ -65,7 +77,7 @@ PNThermalRadiation::computeQpResidual()
   const auto res_leading_order = -n_order_plus_2 / sigma_n_plus_1_interpolated * grad_phi_n_plus_2_normal;
 
   // Building the residual for the leagged order
-  const auto n_order_minus_2 = _n*(_n-1)/((2*_n+1)*(2*_n-1));
+  const auto n_order_minus_2 = rn*(rn-1)/((2*rn+1)*(2*rn-1));
   ADReal sigma_n_minus_1_interpolated;
   interpolate(_coeff_interp_method,
     sigma_n_minus_1_interpolated,
@@ -77,8 +89,8 @@ PNThermalRadiation::computeQpResidual()
   const auto res_lagged_order = -n_order_minus_2 / sigma_n_minus_1_interpolated * grad_phi_n_minus_2_normal;
 
   // Building the residual for the current term
-  const auto current_n_order_plus_2 = Utility::pow<2>(_n+1)/((2*_n+1)*(2*_n+3));
-  const auto current_n_order_minus_2 = Utility::pow<2>(_n)/((2*_n+1)*(2*_n-1));
+  const auto current_n_order_plus_2 = Utility::pow<2>(rn+1)/((2*rn+1)*(2*rn+3));
+  const auto current_n_order_minus_2 = Utility::pow<2>(rn)/((2*rn+1)*(2*rn-1));
   const auto res_order = -(current_n_order_plus_2/sigma_n_plus_1_interpolated + 
                            current_n_order_minus_2/sigma_n_minus_1_interpolated) * gradUDotNormal(state);
 
